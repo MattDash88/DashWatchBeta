a9c221945e199b567a8f752f7a3edad6ee583dae
MattDash88 <MattDash@protonmail.com>
2019-05-14 18:54:49 +0200

    Changes for cloud deployment

--- cache.js
+++ cache.js
@@ -1,5 +1,11 @@
 const redis = require('redis')
-const client = redis.createClient()
+const redisOptions = {
+  host: "34.74.57.226",
+  port: 6379,
+  password: "foobared",
+}
+const client = redis.createClient(redisOptions)
+
 
 // Log any errors
 client.on('error', function(error) {
