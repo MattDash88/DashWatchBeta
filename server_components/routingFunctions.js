766f17ca6323269f0cb7f70da5747e04ed4480fc
MattDash88 <MattDash@protonmail.com>
2019-07-02 16:54:58 +0200

    Fixed analytics key

--- server_components/routingFunctions.js
+++ server_components/routingFunctions.js
@@ -1,13 +1,21 @@
 // All server code uses ES5 because of some packages
 const fetch = require('isomorphic-unfetch');
+const ReactGA = require('react-ga');
+require('dotenv').config()
+var gaKey = process.env.GAKEY
+ReactGA.initialize(gaKey);
 
 // Function to return the record when the info of a single proposal is requested
+
 var reportRedirects = function handleReportRedirects(month, proposalID) {
+    // Sending (anonymous) pageview request to Analytics
+    var x = Math.floor((Math.random() * 100000) + 1);   // Random number to avoid caching
     fetch(`https://www.google-analytics.com/collect?v=1&tid=${gaKey}&cid=4B8302DA-21AD-401F-AF45-1DFD956B80B5&sc=end&t=pageview&dp=%2F/r/${req.params.month}/${req.params.reportId}&z=${x}`,
       {
         method: 'post',
       }
     )
+
     try {
         if (proposalID.match('.pdf')) {
             const actualPage = `https://reports.dashwatch.org/${month}/${proposalID}`
