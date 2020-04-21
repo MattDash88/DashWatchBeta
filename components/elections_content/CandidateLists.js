a2af382b6dd97ec9133e1a7672be7e2072e473d4
MattDash88 <MattDash@protonmail.com>
2020-04-22 01:35:44 +0200

    Changes reflecting new application deadline

--- components/elections_content/CandidateLists.js
+++ components/elections_content/CandidateLists.js
@@ -85,8 +85,8 @@ class CandidateLists extends React.Component {
                 </section>
                 <section className="tpPageTopSection" value={electionId == "TPE2020" ? "Active" : "Inactive"}>
                 <h1 className="tpHeader">2020 Dash Trust Protector Candidates</h1>
-                <p className="tpText">Voting for the Trust Protectors 2020 Elections is currently in progress, the voting app is available at <a className="tpHowToLink" id="results" href={`https://tpe2020.dashwatch.org`}>tpe2020.dashwatch.org</a>.</p>
-                <p className="tpText">The elections will end on Thursday April 30th at 23.59 UTC.</p>
+                <p className="tpText">The application deadline for the Trust Protectors 2020 Elections has been extended and will now close on Thursday April 24, 2020 at 23.59 UTC.</p>
+                <p className="tpText">One or two days after the application deadline is closed the voting app will be available at <a className="tpHowToLink" id="results" href={`https://tpe2020.dashwatch.org`}>tpe2020.dashwatch.org</a>.</p>
                 <div className="electionsIndexWrapper">
                     <div className="electionsIndexItemFirst"><p className="tpColumnTitle">Candidate</p></div>
                     <div className="electionsIndexItem"><p className="tpColumnTitle">Contact</p></div>
