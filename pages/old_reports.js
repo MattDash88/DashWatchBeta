bc2e06eacf6c9c1e9c3d736aa1d16fd79527e557
MattDash88 <MattDash@protonmail.com>
2020-12-21 11:23:11 +0100

    December 2020 Update

--- pages/old_reports.js
+++ pages/old_reports.js
@@ -240,6 +240,7 @@ class Month extends React.Component {
                                                             <button id="dropdownMenu" value="May" className="dropdownItem" onClick={this.handleSelectMonth}>May {this.state.yearId}</button>
                                                             <button id="dropdownMenu" value="June" className="dropdownItem" onClick={this.handleSelectMonth}>June {this.state.yearId}</button>
                                                             <button id="dropdownMenu" value="July" className="dropdownItem" onClick={this.handleSelectMonth}>July {this.state.yearId}</button>
+                                                            <button id="dropdownMenu" value="August" className="dropdownItem" onClick={this.handleSelectMonth}>August {this.state.yearId}</button>
                                                         </div>
                                                     ) : (
                                                             <div className="dropdownMenu" id="dropdownMenu">
